a464a87f2bbc5d104a3ec03b85423cbd85c58811
katherinelef <katherine.lef@hotmail.com>
2018-03-12 18:36:17 -0500

    añadiendo header

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../App.css';
+const Header = () => {
+  return (
+    <div>
+      <nav className="navbar navbar-light">
+        <img src="https://image.ibb.co/hwERf7/burguer.png"  width="110" height="110" className="d-inline-block align-top" alt="logoBurguer"/>
+        <h1>Burguer Queen</h1>
+      </nav>
+    </div>
+  );
+};
+
+export default Header
